a7d772b28dc37759dc47dd41450159adca23a3b8
sean <997647204@qq.com>
2018-07-11 16:02:56 +0800

    proj4

---
+++ sp17/project/exploration/tests/q1.py
@@ -0,0 +1,25 @@
+
+test = {
+  'name': 'Check file name',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> # Make sure that you name your submission file exploration.ipynb
+          >>> import os
+          >>> os.path.exists('exploration.ipynb')
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
